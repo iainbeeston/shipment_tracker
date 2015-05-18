cf9d94779b652281a892c769fdc4f77bbfd33dfc
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-05-18 17:11:46 +0100

    Use #find_each to go through Events
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,17 @@
 class Event < ActiveRecord::Base
+  class BatchedRelation
+    delegate :to_a, to: :@relation
+
+    def initialize(relation)
+      @relation = relation
+    end
+
+    def each(&block)
+      @relation.find_each(&block)
+    end
+  end
+
   def self.in_order_of_creation
-    order(created_at: :asc)
+    BatchedRelation.new(order(created_at: :asc))
   end
 end
