8181732acb9301f2afdcea84b01e9b0dae753f74
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-05-20 14:44:04 +0100

    remove app prefetching on boot
    Signed-off-by: Theo Cushion <tcushion@pivotal.io>

--- config/puma.rb
+++ config/puma.rb
@@ -12,9 +12,4 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-
-  RepositoryLocation.all.pluck(:name).each do |app_name|
-    Rails.logger.info "Loading #{app_name}"
-    GitRepositoryLoader.from_rails_config.load(app_name)
-  end
 end
