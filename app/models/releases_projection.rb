1468ebb8ab7a50f1e94bb5abb0fafddf6fbba658
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-05-29 11:26:10 +0100

    Refactor ReleasesProjection
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- app/models/releases_projection.rb
+++ app/models/releases_projection.rb
@@ -11,13 +11,18 @@ class ReleasesProjection
   end
 
   def apply_all(events)
-    events.each { |e| apply(e) }
+    events.each do |event|
+      apply(event)
+    end
+
+    associate_dependent_releases_with_feature_review
   end
 
   def apply(event)
     case event
     when JiraEvent
-      apply_jira_event(event)
+      update_ticket_status(event)
+      associate_releases_with_feature_review(event)
     end
   end
 
@@ -25,13 +30,15 @@ class ReleasesProjection
     @commits ||= @git_repository.recent_commits(@per_page)
   end
 
-  def releases
+  def associate_dependent_releases_with_feature_review
     @releases_hash.keys.each do |sha|
       @git_repository.get_dependents(sha).each do |dependent_commit|
         @releases_hash[dependent_commit.id] = @releases_hash[sha]
       end
     end
+  end
 
+  def releases
     commits.map { |commit|
       release_hash = @releases_hash.fetch(commit.id, {})
       Release.new(
@@ -44,29 +51,32 @@ class ReleasesProjection
 
   private
 
-  def apply_jira_event(event)
-    if @tickets_hash.key?(event.issue_id)
-      @tickets_hash[event.issue_id] = event.status
-    end
-
-    URI.extract(event.comment)
-      .map { |comment_url| Addressable::URI.parse(comment_url) }
-      .select { |uri| uri.path == '/feature_reviews' }
-      .each { |uri|
-        shas = extract_shas_from_uri(uri)
-        shas.each do |sha|
-          if commits.find { |c| c.id == sha }
-            @releases_hash[sha] = { issue_id: event.issue_id, path: uri.request_uri }
-            @tickets_hash[event.issue_id] = event.status
-          end
-        end
-      }
+  def update_ticket_status(jira_event)
+    @tickets_hash[jira_event.issue_id] = jira_event.status if @tickets_hash.key?(jira_event.issue_id)
   end
 
-  def extract_shas_from_uri(uri)
+  def associate_releases_with_feature_review(jira_event)
+    uris = extract_relevant_uris_from_comment(jira_event.comment)
+    uris.each do |uri|
+      commit_oids = extract_relevant_commit_oids_from_uri(uri)
+      commit_oids.each do |commit_oid|
+        @releases_hash[commit_oid] = { issue_id: jira_event.issue_id, path: uri.request_uri }
+        @tickets_hash[jira_event.issue_id] = jira_event.status
+      end
+    end
+  end
+
+  def extract_relevant_uris_from_comment(comment)
+    URI.extract(comment)
+      .map { |comment_url| Addressable::URI.parse(comment_url) }
+      .select { |uri| uri.path == '/feature_reviews' }
+  end
+
+  def extract_relevant_commit_oids_from_uri(uri)
     uri.query_values
       .select { |key, _value| key.start_with?('apps[') }
       .values
       .reject(&:empty?)
+      .select { |commit_oid| commits.find { |c| c.id == commit_oid } }
   end
 end
