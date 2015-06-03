3faee528516c5c7e1ec530841b361485bfba98bf
Andi Studer <andi.studer@fundingcircle.com>
2015-06-03 16:24:28 +0100

    expand unicorn config to listen on tmp/sockets/unicorn.sock
    Signed-off-by: Theo Cushion <tcushion@pivotal.io>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,14 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1)
 timeout 15
 preload_app true
 
+root = File.expand_path('../..', __FILE__)
+
+stderr_path File.join(root, 'log/unicorn.log')
+stdout_path File.join(root, 'log/unicorn.log')
+
+pid File.join(root, 'tmp/pids/unicorn.pid')
+listen File.join(root, 'tmp/sockets/unicorn.sock'), backlog: 64
+
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     Rails.logger.warn 'Unicorn master intercepting TERM and sending myself QUIT instead'
