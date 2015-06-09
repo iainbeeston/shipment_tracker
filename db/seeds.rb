d0887cf672fafc9dc572dc71d63affaea737e209
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-06-09 10:43:12 +0100

    remove seeds as not relevant in production and not required in test envs
    Signed-off-by: Andi Studer <andi.studer@fundingcircle.com>

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-RepositoryLocation.find_or_create_by(name: 'shipment_tracker') do |repository_location|
-  repository_location.uri = 'https://github.com/FundingCircle/shipment_tracker.git'
-end
-
-RepositoryLocation.find_or_create_by(name: 'hello_world_rails') do |repository_location|
-  repository_location.uri = 'https://github.com/FundingCircle/hello_world_rails.git'
-end
