8056d10ec2776f5f2d6fe382560dc20a14fb565d
Dennis Ideler <dennis.ideler@fundingcircle.com>
2015-06-18 11:03:16 +0100

    Make login URLs more readable
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,20 @@ module SessionsHelper
   end
 
   def login_url
-    ENV.fetch(
-      'AUTH_LOGIN_URL',
-      "https://#{ENV['AUTH0_DOMAIN']}/authorize?response_type=code&scope=openid%20profile"\
-      "&client_id=#{ENV['AUTH0_CLIENT_ID']}"\
-      "&redirect_uri=#{request.protocol}#{request.host_with_port}"\
-      "/auth/auth0/callback&connection=#{Rails.application.config.x.auth0_connection}",
-    )
+    ENV.fetch('AUTH_LOGIN_URL', auth0_login_url)
+  end
+
+  def auth0_login_url
+    auth0_domain = ENV.fetch('AUTH0_DOMAIN')
+    auth0_client_id = ENV['AUTH0_CLIENT_ID']
+    auth0_connection = Rails.application.config.x.auth0_connection
+    redirect_uri = "#{request.protocol}#{request.host_with_port}/auth/auth0/callback"
+
+    "https://#{auth0_domain}/authorize"\
+      '?response_type=code'\
+      '&scope=openid%20profile'\
+      "&client_id=#{auth0_client_id}"\
+      "&redirect_uri=#{redirect_uri}"\
+      "&connection=#{auth0_connection}"
   end
 end
