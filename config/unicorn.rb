f56ad4658698ee1c6f9adde38c608a5fcfca8b4a
Andi Studer <andi.studer@fundingcircle.com>
2015-06-30 13:24:40 +0100

    allow protecting STDOUT and STDERR default

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,14 +2,16 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1)
 timeout 15
 preload_app true
 
-root = File.expand_path('..', __dir__)
-paths = {
-  stderr: File.join(root, 'log/production.log'),
-  stdout: File.join(root, 'log/production.log'),
-}
+unless ENV['PROTECT_STDOUT'] == 'true'
+  root = File.expand_path('..', __dir__)
+  paths = {
+    stderr: File.join(root, 'log/production.log'),
+    stdout: File.join(root, 'log/production.log'),
+  }
 
-stderr_path paths.fetch(:stderr)
-stdout_path paths.fetch(:stdout)
+  stderr_path paths.fetch(:stderr)
+  stdout_path paths.fetch(:stdout)
+end
 
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
