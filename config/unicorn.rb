cc54b1c12fb999e85d43ea9f69968010e3e7b4e8
Andi Studer <andi.studer@fundingcircle.com>
2015-06-30 12:19:36 +0100

    remove custom path for stdout and stderr

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,15 +2,6 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1)
 timeout 15
 preload_app true
 
-root = File.expand_path('..', __dir__)
-paths = {
-  stderr: File.join(root, 'log/production.log'),
-  stdout: File.join(root, 'log/production.log'),
-}
-
-stderr_path paths.fetch(:stderr)
-stdout_path paths.fetch(:stdout)
-
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     Rails.logger.warn 'Unicorn master intercepting TERM and sending myself QUIT instead'
