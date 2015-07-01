f599b09cdb4b02911c8291f5984c6549b81f39a0
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-07-01 16:39:39 +0100

    Don't test Omniauth :)
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Authentication' do
   describe 'auth0' do
     context 'when not logged in' do
-      let(:callback_url) { 'http://www.example.com/auth/auth0/callback' }
-
       it 'redirects to auth0' do
         get '/feature_reviews/new'
 
         expect(response).to redirect_to('/auth/auth0')
-
         follow_redirect!
 
         oauth_url = response['Location']
@@ -17,13 +14,7 @@ RSpec.describe 'Authentication' do
 
         expect(oauth_url_query['connection']).to eq('Username-Password-Authentication')
         expect(oauth_url_query['response_type']).to eq('code')
-        expect(oauth_url_query['redirect_uri']).to eq(callback_url)
-      end
-
-      it 'redirects to failure if callback url does not have token' do
-        get callback_url
-
-        expect(response).to redirect_to('/auth/failure?message=invalid_credentials&strategy=auth0')
+        expect(oauth_url_query['redirect_uri']).to end_with('/auth/auth0/callback')
       end
     end
   end
