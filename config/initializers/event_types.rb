33d75a0ec8c8f325a7f8ee0b68b76f7494d78c01
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-07-15 17:03:18 +0100

    Disable CircleCi event - only need CircleCiManual
    Signed-off-by: Theo Cushion <tcushion@pivotal.io>

--- config/initializers/event_types.rb
+++ config/initializers/event_types.rb
@@ -1,7 +1,8 @@
 # rubocop:disable Metrics/LineLength
 Rails.application.config.event_types = [
-  EventType.new(name: 'CircleCI', endpoint: 'circleci', event_class: CircleCiEvent),
-  EventType.new(name: 'CircleCI (manual)', endpoint: 'circleci-manual', event_class: CircleCiManualWebhookEvent),
+  # Waiting on CircleCI to support SNI on SSL webhooks
+  # EventType.new(name: 'CircleCI (webhook)', endpoint: 'circleci', event_class: CircleCiEvent),
+  EventType.new(name: 'CircleCI', endpoint: 'circleci-manual', event_class: CircleCiManualWebhookEvent),
   EventType.new(name: 'Deployment', endpoint: 'deploy', event_class: DeployEvent),
   EventType.new(name: 'Jenkins', endpoint: 'jenkins', event_class: JenkinsEvent),
   EventType.new(name: 'JIRA', endpoint: 'jira', event_class: JiraEvent),
