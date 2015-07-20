c61c5b16bc2978395b57066de163c2602a079175
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-07-20 17:27:29 +0100

    Rename RepositoryLocation.find_by_fuzzy_url to find_by_github_ssh_url and make it private

--- app/models/repository_location.rb
+++ app/models/repository_location.rb
@@ -4,13 +4,14 @@ class RepositoryLocation < ActiveRecord::Base
   end
 
   def self.update_from_github_notification(payload)
-    repository_location = find_by_fuzzy_url(payload['repository']['ssh_url'])
+    repository_location = find_by_github_ssh_url(payload['repository']['ssh_url'])
     return unless repository_location
     repository_location.update(remote_head: payload['after'])
   end
 
-  def self.find_by_fuzzy_url(url)
+  def self.find_by_github_ssh_url(url)
     path = Addressable::URI.parse(url).path
     find_by('uri LIKE ?', "%#{path}")
   end
+  private_class_method :find_by_github_ssh_url
 end
