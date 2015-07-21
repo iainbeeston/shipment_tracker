4af9fb057815cbc1223ac7cdc88be78d653dbd8b
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-07-21 17:55:23 +0100

    Log whenever cron jobs
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 every 10.minutes, roles: [:cron_events_worker] do
-  rake 'jobs:update_events'
+  rake 'jobs:update_events', output: File.join(path, 'log', 'jobs.log')
 end
 
 every 10.minutes, roles: [:cron] do
-  rake 'jobs:update_git'
+  rake 'jobs:update_git', output: File.join(path, 'log', 'jobs.log')
 end
