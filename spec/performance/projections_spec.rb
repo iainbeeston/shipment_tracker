831ee2c662d97c55d0ed7f9ae71d555c7e53a459
Giuseppe Capizzi <gcapizzi@pivotal.io>
2015-07-21 16:29:23 +0100

    Add performance tests for Feature Review Search
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- spec/performance/projections_spec.rb
+++ spec/performance/projections_spec.rb
@@ -8,25 +8,56 @@ require 'csv'
 
 RSpec.describe 'Projection performance', type: :request do
   before(:all) do
-    DatabaseCleaner.strategy = :truncation
     login_with_omniauth
   end
 
-  def benchmark(count, &block)
+  def create_events(count)
     number = (count / 6).to_i
 
-    Benchmark.realtime do
-      build_events(type: :jira_event, number: number, key: 'JIRA-$ID')
-      build_events(type: :circle_ci_event, number: number)
-      build_events(type: :jenkins_event, number: number)
-      build_events(type: :deploy_event, number: number)
-      build_events(type: :manual_test_event, number: number)
-      build_events(type: :uat_event, number: number)
-    end
+    build_events(type: :jira_event, number: number, key: 'JIRA-$ID')
+    build_events(type: :circle_ci_event, number: number)
+    build_events(type: :jenkins_event, number: number)
+    build_events(type: :deploy_event, number: number)
+    build_events(type: :manual_test_event, number: number)
+    build_events(type: :uat_event, number: number)
+  end
+
+  def drop_events
+    Event.delete_all
+  end
+
+  def create_snapshots
+    Repositories::FeatureReviewRepository.new.update
+  end
+
+  def drop_snapshots
+    Snapshots::FeatureReview.delete_all
+    Snapshots::EventCount.delete_all
+  end
+
+  def benchmark(count, &block)
+    create_events(count)
+
     time_to_run = Benchmark.realtime(&block)
     results = [Event.count, time_to_run]
-    puts results.inspect
-    Event.delete_all
+    puts "#{results[0]} events -> #{results[1].round(3)} seconds"
+
+    drop_events
+
+    results
+  end
+
+  def benchmark_with_snapshots(count, &block)
+    create_events(count)
+    create_snapshots
+
+    time_to_run = Benchmark.realtime(&block)
+    results = [Event.count, time_to_run]
+    puts "#{results[0]} events -> #{results[1].round(3)} seconds"
+
+    drop_events
+    drop_snapshots
+
     results
   end
 
@@ -37,7 +68,6 @@ RSpec.describe 'Projection performance', type: :request do
       csv_contents << headers
       block.call(csv_contents)
     end
-    puts File.read(filename)
   end
 
   def progression(start: 10_000, points: 10, factor: 2, &block)
@@ -55,22 +85,70 @@ RSpec.describe 'Projection performance', type: :request do
     it 'measures the request time' do
       csv(name: 'feature_review', headers: ['Count', 'Time to Run']) do |file|
         progression do |event_count|
-          DatabaseCleaner.cleaning do
-            create :jira_event, comment_body: "Here you go: #{feature_review_url}"
-            create :circle_ci_event, version: apps['frontend']
-            apps.each do |name, version|
-              create :deploy_event, server: server, app_name: name, version: version
-            end
-            create :manual_test_event, apps: apps.map { |name, version| { name: name, version: version } }
-            create :uat_event, server: server
+          create :jira_event, comment_body: "Here you go: #{feature_review_url}"
+          create :circle_ci_event, version: apps['frontend']
+          apps.each do |name, version|
+            create :deploy_event, server: server, app_name: name, version: version
+          end
+          create :manual_test_event, apps: apps.map { |name, version| { name: name, version: version } }
+          create :uat_event, server: server
 
-            file << benchmark(event_count) do
-              get(feature_review_path)
+          file << benchmark(event_count) do
+            get(feature_review_path)
+          end
+        end
+      end
+    end
+  end
+
+  describe 'Feature Review Search' do
+    let(:apps) { { 'frontend' => version, 'backend' => 'def' } }
+    let(:server) { 'uat.fc.com' }
+    let(:feature_review_url) { Support::FeatureReviewUrl.build(apps, server) }
+
+    let(:repo_name) { 'frontend' }
+    let(:test_git_repo) { Support::GitTestRepository.new }
+    let(:repository_builder) { Support::RepositoryBuilder.new(test_git_repo) }
+
+    before do
+      repository_builder.build(git_diagram)
+      RepositoryLocation.create(name: repo_name, uri: "file://#{test_git_repo.dir}")
+    end
+
+    # rubocop:disable Style/BlockDelimiters
+    context 'with a simple repo so that git has minimal effect on performance' do
+      let(:git_diagram) { '-A' }
+      let(:version) { test_git_repo.commit_for_pretend_version('A') }
+
+      context 'without snapshots' do
+        it 'measures the request time' do
+          csv(name: 'feature_review_search', headers: ['Count', 'Time to Run']) do |file|
+            progression do |event_count|
+              create :jira_event, comment_body: "Here you go: #{feature_review_url}"
+
+              file << benchmark(event_count) do
+                get(search_feature_reviews_path, application: 'frontend', version: version)
+              end
+            end
+          end
+        end
+      end
+
+      context 'with snapshots' do
+        it 'measures the request time' do
+          csv(name: 'feature_review_search_with_snapshots', headers: ['Count', 'Time to Run']) do |file|
+            progression do |event_count|
+              create :jira_event, comment_body: "Here you go: #{feature_review_url}"
+
+              file << benchmark_with_snapshots(event_count) do
+                get(search_feature_reviews_path, application: 'frontend', version: version)
+              end
             end
           end
         end
       end
     end
+    # rubocop:enable Style/BlockDelimiters
   end
 
   describe 'Releases' do
