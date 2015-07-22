dab13ac6c827a93521c92f825db61fdcfed05511
Andi Studer <andi.studer@fundingcircle.com>
2015-07-22 11:19:39 +0100

    require authentication on github notification endpoint
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- app/controllers/github_notifications_controller.rb
+++ app/controllers/github_notifications_controller.rb
@@ -1,8 +1,15 @@
 class GithubNotificationsController < ApplicationController
-  skip_before_action :require_authentication
+  skip_before_action :verify_authenticity_token
 
   def create
     RepositoryLocation.update_from_github_notification(request.request_parameters)
     head :ok
+  end
+
+  private
+
+  def unauthenticated_strategy
+    self.status = 403
+    self.response_body = 'Forbidden'
   end
 end
