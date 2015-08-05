0649a0c857cbb06e347f83e68f938072d5798349
Theo Cushion <tcushion@pivotal.io>
2015-08-05 16:06:19 +0100

    Add warning about order important to repositories
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- config/initializers/respositories.rb
+++ config/initializers/respositories.rb
@@ -4,5 +4,7 @@ Rails.configuration.repositories = [
   Repositories::BuildRepository.new,
   Repositories::ManualTestRepository.new,
   Repositories::TicketRepository.new,
+  # This must always be last as it depends on DeployRepository.
+  # Until we make snapshot updating more robust (e.g. jobs queue or table locking) this will have to remain.
   Repositories::UatestRepository.new,
 ]
