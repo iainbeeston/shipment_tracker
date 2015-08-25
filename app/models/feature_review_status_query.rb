b705abb303312533882dda1f34439c30c4fd46e0
Neil Atkinson <natkinson@NATKINSON.local>
2015-08-25 09:57:47 +0100

    Whip - FeatureReviewStatusQuery.

---
+++ app/models/feature_review_status_query.rb
@@ -0,0 +1,15 @@
+class FeatureReviewStatusQuery
+  def initialize(feature_review, at:)
+    @feature_review = feature_review
+    @ticket_repository = Repositories::TicketRepository.new
+    @time = at
+  end
+
+  def approved?
+    tickets.all?(&:approved?)
+  end
+
+  def tickets
+    @tickets ||= ticket_repository.tickets_for(projection_url: feature_review.url, at: time)
+  end
+end
