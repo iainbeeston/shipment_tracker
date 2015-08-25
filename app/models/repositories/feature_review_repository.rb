29f0c3626422e1dc4e56c8bd0342f4799e7c8edb
Neil Atkinson <natkinson@NATKINSON.local>
2015-08-25 14:56:35 +0100

    FeatureReviewRepository returns snapshots at specified times.

--- app/models/repositories/feature_review_repository.rb
+++ app/models/repositories/feature_review_repository.rb
@@ -9,14 +9,19 @@ module Repositories
 
     delegate :table_name, to: :store
 
-    def feature_reviews_for(versions)
-      store.where('versions && ARRAY[?]::varchar[]', versions).group_by(&:url).map { |_, snapshots|
-        latest_snapshot = snapshots.max(&:event_created_at)
-        Factories::FeatureReviewFactory.new.create(
-          url: latest_snapshot.url,
-          versions: latest_snapshot.versions,
-        )
-      }
+    def feature_reviews_for(versions, at: nil)
+      query = at ? store.arel_table['event_created_at'].lteq(at) : nil
+
+      store
+        .where('versions && ARRAY[?]::varchar[]', versions)
+        .where(query)
+        .group_by(&:url).map { |_, snapshots|
+          most_recent_snapshot = snapshots.max(&:event_created_at)
+          Factories::FeatureReviewFactory.new.create(
+            url: most_recent_snapshot.url,
+            versions: most_recent_snapshot.versions,
+          )
+        }
     end
 
     def apply(event)
